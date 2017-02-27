825075b0327e4e4ebc776bc8367ce1f4db7a2ab8
ModernBitard <vlad.gudkov.99@gmail.com>
2017-02-27 16:38:14 +0300

    updated main page,delete unnecessary, change logo, add rus language for several messages

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return view('home');
+        if(Auth::check()){
+            return redirect('/home');
+        }
+        return view('welcome');
+    }
+
+    public function home()
+    {
+        if(Auth::check()){
+            return view('home');
+        }
+        return redirect('/');
+    }
+
+    public function registration()
+    {
+        return view('');
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>ToolPass</title>
-    
+
     {{--Styles--}}
     <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection"/>
     <style>
@@ -19,58 +19,18 @@
         }
 
         .logo {
-            margin-top: 150px;
+            margin: 1em auto;
+        }
+
+        body {
+            background-color: #f5f5f5;
         }
     </style>
 </head>
 <body>
 
-{{--MODAL LOGIN--}}
-{{------------------}}
-<div id="login" class="modal">
-    <div class="modal-content">
-
-        <h4>Войти</h4>
-        <br>
-
-        <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-            {{ csrf_field() }}
-            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                <div class="input-field">
-                    <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}"
-                           required>
-                    <label for="email">E-Mail адресс</label>
-                    @if ($errors->has('email'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                    @endif
-                </div>
-            </div>
-
-            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                <div class="input-field">
-                    <input id="password" type="password" class="validate" name="password" required>
-                    <label for="password">Пароль</label>
-                    @if ($errors->has('password'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                    @endif
-                </div>
-            </div>
-
-            <div class="modal-footer">
-                <button type="submit" class="btn">
-                    Login
-                </button>
-            </div>
-        </form>
-    </div>
-</div>
 
 {{--MODAL SIGNUP--}}
-{{------------------}}
 <div id="sign_up" class="modal">
     <div class="modal-content">
 
@@ -84,7 +44,7 @@
                 <div class="input-field">
                     <input id="name" type="text" class="validate" name="name" value="{{ old('name') }}"
                            required>
-                    <label for="name">Логин</label>
+                    <label for="name" data-error="не верно" data-success="верно">Логин</label>
                     @if ($errors->has('name'))
                         <span class="help-block">
                             <strong>{{ $errors->first('name') }}</strong>
@@ -97,7 +57,7 @@
                 <div class="input-field">
                     <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}"
                            required>
-                    <label for="email">E-Mail адресс</label>
+                    <label for="email" data-error="не верно" data-success="верно">E-Mail адресс</label>
                     @if ($errors->has('email'))
                         <span class="help-block">
                             <strong>{{ $errors->first('email') }}</strong>
@@ -135,28 +95,59 @@
 </div>
 
 {{------LOGO-----}}
-{{---------------}}
 <div class="logo center-align">
     <img src="img/logo-v2.png" alt="logo_toolpass">
 </div>
 
 {{--LOG IN / Sign UP--}}
-{{--------------------}}
 <div class="container">
     <div class="row">
+        <h5 class="center-align">Войдите чтобы пользоваться ToolPass</h5>
         <div class="forms">
-            <div class="col l4 s12 offset-l2">
-                <a href="#login">
-                    <div class="card-panel hoverable center-align">
-                        <h5>Войти</h5>
-                    </div>
-                </a>
+            <div class="col l4 s12 offset-l4">
+
+                <div class="card-panel hoverable ">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <div class="input-field">
+                                <input id="email" type="email" class="validate" name="email" value="{{ old('email') }}"
+                                       required>
+                                <label for="email" data-error="не верно" data-success="верно">Введите
+                                    адресс электронной почты</label>
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="input-field">
+                                <input id="password" type="password" class="validate" name="password" required>
+                                <label for="password" data-error="не верно" data-success="верно">Введите Пароль</label>
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="modal-footer">
+                            <button type="submit" class="btn" style="width: 100%;">
+                                Войти
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
 
-            <div class="col l4 s12">
-                <a href="#sign_up">
+            <div class="col l4 s12 offset-l4">
+                <a href="{{url('/registration')}}">
                     <div class="card-panel hoverable center-align">
-                        <h5>Зарегистрироваться</h5>
+                        <h5>Создать аккаунт</h5>
                     </div>
                 </a>
             </div>
