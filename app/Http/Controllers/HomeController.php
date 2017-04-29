2d0d9b32e5fb1aae6b2c177006fdef21298dba9c
DrVladislav <vlad.gudkov.99@gmail.com>
2017-04-29 14:10:30 +0300

    finished with home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,21 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use Carbon\Carbon;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        if(Auth::check()){
+        if (Auth::check()) {
             return redirect('/home');
         }
-        return view('welcome');
+        return view('welcome', ['today' => Carbon::today()]);
     }
 
     public function home()
     {
-        if(Auth::check()){
-            return view('home');
+        if (Auth::check()) {
+            $services = Auth::user()->services;
+            return view('layouts.app', compact('services'));
         }
         return redirect('/');
     }
