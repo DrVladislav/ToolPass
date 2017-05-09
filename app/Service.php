dfe1c9c176b4432df52943cd4d1f56a79aae3f96
DrVladislav <vlad.gudkov.99@gmail.com>
2017-05-10 01:36:47 +0300

    added fillable rows

--- app/Service.php
+++ app/Service.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
+    protected $fillable = ['service', 'login', 'email', 'password', 'others'];
+
     public function user()
     {
         return $this->belongsTo(User::class);
